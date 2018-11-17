7a2468e5c2e3adf01948930795d8df8bb2eec495
Serhii Usatenko <winsento.otto@gmail.com>
2018-11-17 20:02:53 +0200

    back to prev version

--- Angular/angular-meteorite-tracker/src/app/dashboard/dashboard.component.ts
+++ Angular/angular-meteorite-tracker/src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from "@angular/core";
 import { MeteoriteService } from "../services/meteorite.service";
-import { FilterService } from "../services/filter.service";
 
 @Component({
   selector: "app-dashboard",
@@ -9,21 +8,21 @@ import { FilterService } from "../services/filter.service";
 })
 export class DashboardComponent implements OnInit {
   public meteoritesList = [];
-  public importanceList;
+  public importanceList = [];
   onlyFell: boolean;
   onlyImportant: boolean;
   path: string[] = ["meteorite"];
   order: number = 1;
 
   constructor(
-    private _meteoriteService: MeteoriteService,
-    private _filterService: FilterService
+    private _meteoriteService: MeteoriteService
+    // private _filterService: FilterService
   ) { }
 
   ngOnInit() {
     this._meteoriteService
       .getMeteorites()
-      .subscribe(data => (this.meteoritesList = data));
+      .subscribe(data => (this.meteoritesList = this.sortData(data)));
 
 
     // this._filterService.onlyFell.subscribe(
@@ -33,16 +32,16 @@ export class DashboardComponent implements OnInit {
     //   onlyImportant => (this.onlyImportant = onlyImportant)
     // );
 
-    this._filterService.importanceList.subscribe(
-      importanceList => (this.importanceList = importanceList)
-    );
+    // this._filterService.importanceList.subscribe(
+    //   importanceList => (this.importanceList = importanceList)
+    // );
   }
 
   sortColumn(prop: string) {
     this.path = prop.split(".");
     this.order = this.order * -1;
     return false; // do not reload
-  }
+  };
 
   setImportance(meteorite) {
     // console.log("id:", meteorite.id);
@@ -55,6 +54,40 @@ export class DashboardComponent implements OnInit {
     }
 
     return false;
-  }
+  };
+
+  sortData(data) {
+    if (this.onlyFell) {
+      const sortedData = [];
+      data.forEach(meteorite => {
+        if (meteorite.fall === "Fell") {
+          sortedData.push(meteorite);
+        } else {
+          return false;
+        }
+      });
+      console.log("result =", sortedData);
+      return sortedData;
+    } else if (this.onlyImportant) {
+      const sortedData = [];
+      data.forEach(meteorite => {
+        if (
+          this.importanceList.length > 0 &&
+          this.importanceList.includes(meteorite.id)
+        ) {
+          console.log("meteorite pushed");
+          sortedData.push(meteorite);
+        } else {
+          // show message that there are no values
+          console.log("no important meteoites");
+          return false;
+        }
+      });
+      console.log("sortedData =", sortedData);
+      return sortedData;
+    } else {
+      return data;
+    }
+  };
 
 }
--- Angular/angular-meteorite-tracker/src/app/filter/filter.component.ts
+++ Angular/angular-meteorite-tracker/src/app/filter/filter.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from "@angular/core";
-import { MeteoriteService } from "../services/meteorite.service";
-import { FilterService } from "../services/filter.service";
 
 @Component({
   selector: "app-filter",
@@ -8,61 +6,9 @@ import { FilterService } from "../services/filter.service";
   styleUrls: ["./filter.component.css"]
 })
 export class FilterComponent implements OnInit {
-  public meteoritesList = [];
-  public importanceList;
-  // public data = [];
-  onlyFell: boolean;
-  onlyImportant: boolean;
 
-  constructor(
-    private _meteoriteService: MeteoriteService,
-    private _filterService: FilterService
-  ) { }
+  constructor() { }
 
-  ngOnInit() {
-
-    this._meteoriteService
-      .getMeteorites()
-      .subscribe(meteoritesList => (this.meteoritesList = meteoritesList));
-
-    this._filterService.importanceList.subscribe(
-      importanceList => (this.importanceList = importanceList)
-    );
-  }
-
-  sortData() {
-    let data = this.meteoritesList;
-    if (this.onlyFell) {
-      const sortedData = [];
-      data.forEach(meteorite => {
-        if (meteorite.fall === "Fell") {
-          sortedData.push(meteorite);
-        } else {
-          return false;
-        }
-      });
-      console.log("result =", sortedData);
-      return sortedData;
-    } else if (this.onlyImportant) {
-      const sortedData = [];
-      data.forEach(meteorite => {
-        if (
-          this.importanceList.length > 0 &&
-          this.importanceList.includes(meteorite.id)
-        ) {
-          console.log("meteorite pushed");
-          sortedData.push(meteorite);
-        } else {
-          // show message that there are no values
-          console.log("no important meteoites");
-          return false;
-        }
-      });
-      console.log("sortedData =", sortedData);
-      return sortedData;
-    } else {
-      return data;
-    }
-  }
+  ngOnInit() { }
 
 }
